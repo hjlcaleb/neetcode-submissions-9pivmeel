39ec6f01a8172c51956e1299e01c6e023e63c09f
Caleb Hu <calebhu552@gmail.com>
2026-04-14 15:39:08 -0700

    Add: design-in-memory-file-system - submission-2

---
+++ Data Structures & Algorithms/design-in-memory-file-system/submission-2.java
@@ -0,0 +1,86 @@
+class FileSystem {
+    private class Entry {
+        Map<String, Entry> children;
+        String content;
+        public Entry() {
+            children = new HashMap<>();
+            content = null;
+        }
+    }
+
+    private Entry root;
+
+    public FileSystem() {
+        root = new Entry();
+    }
+    
+    public List<String> ls(String path) {
+        Entry cur = root;
+        String[] components = path.split("/");
+        for (int i = 1; i < components.length; i++) {
+            String curComponent = components[i];
+            if (!cur.children.containsKey(curComponent)) {
+                cur.children.put(curComponent, new Entry());
+            }
+            cur = cur.children.get(curComponent);
+            if (i == components.length - 1 && cur.content != null) {
+                return Arrays.asList(curComponent);
+            }
+        }
+
+        List<String> children = new ArrayList<>(cur.children.keySet());
+        Collections.sort(children);
+        return children;
+    }
+    
+    public void mkdir(String path) {
+        Entry cur = root;
+        String[] components = path.split("/");
+        for (int i = 1; i < components.length; i++) {
+            String curComponent = components[i];
+            if (!cur.children.containsKey(curComponent)) {
+                cur.children.put(curComponent, new Entry());
+            }
+            cur = cur.children.get(curComponent);
+        }
+    }
+    
+    public void addContentToFile(String filePath, String content) {
+        Entry cur = root;
+        String[] components = filePath.split("/");
+        for (int i = 1; i < components.length; i++) {
+            String curComponent = components[i];
+            if (!cur.children.containsKey(curComponent)) {
+                cur.children.put(curComponent, new Entry());
+            }
+            cur = cur.children.get(curComponent);
+        }
+        if (cur.content != null) {
+            cur.content += content;
+        } else {
+            cur.content = content;
+        }
+    }
+    
+    public String readContentFromFile(String filePath) {
+        Entry cur = root;
+        String[] components = filePath.split("/");
+        for (int i = 1; i < components.length; i++) {
+            String curComponent = components[i];
+            if (!cur.children.containsKey(curComponent)) {
+                cur.children.put(curComponent, new Entry());
+            }
+            cur = cur.children.get(curComponent);
+        }
+        return cur.content;
+    }
+}
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * FileSystem obj = new FileSystem();
+ * List<String> param_1 = obj.ls(path);
+ * obj.mkdir(path);
+ * obj.addContentToFile(filePath,content);
+ * String param_4 = obj.readContentFromFile(filePath);
+ */
