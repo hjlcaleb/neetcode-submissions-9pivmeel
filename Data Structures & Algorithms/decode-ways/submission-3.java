74ecd33bbd8935f0d48245ed25a22fac579c5ab3
Caleb Hu <calebhu552@gmail.com>
2026-05-12 16:09:11 -0700

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[s.length()] = 1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != '0') {
+                dp[i] = dp[i + 1];
+                if (i + 1 < s.length()) {
+                    if (Integer.parseInt(s.substring(i, i + 2)) <= 26) {
+                        dp[i] += dp[i + 2];
+                    }
+                }
+            } else {
+                dp[i] = 0;
+            }
+        }
+        return dp[0];
+    }
+}
