e63cad10181c2f13d2ebfda70284076880e2a34b
Caleb Hu <calebhu552@gmail.com>
2026-05-12 18:56:34 -0700

    Add: maximum-profit-in-job-scheduling - submission-3

---
+++ Data Structures & Algorithms/maximum-profit-in-job-scheduling/submission-3.java
@@ -0,0 +1,44 @@
+class Solution {
+    private int[][] jobs;
+
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        jobs = new int[startTime.length][3];
+        for (int i = 0; i < jobs.length; i++) {
+            jobs[i][0] = startTime[i];
+            jobs[i][1] = endTime[i];
+            jobs[i][2] = profit[i];
+        }
+
+        Arrays.sort(jobs, (a, b) -> Integer.compare(a[0], b[0]));
+        int[] dp = new int[jobs.length + 1];
+        dp[jobs.length] = 0;
+        for (int i = jobs.length - 1; i >= 0; i--) {
+            int[] curJob = jobs[i];
+            int nextValidIdx = nextAvailableJob(curJob, i);
+            if (nextValidIdx == -1) {
+                dp[i] = Math.max(curJob[2], dp[i + 1]);
+            } else {
+                dp[i] = Math.max(curJob[2] + dp[nextValidIdx], dp[i + 1]);
+            }
+        }
+        return dp[0];
+    }
+
+    private int nextAvailableJob(int[] curJob, int start) {
+        int resultIdx = -1;
+        int l = start + 1;
+        int r = jobs.length - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (jobs[mid][0] >= curJob[1]) {
+                resultIdx = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return resultIdx;
+    }
+}
