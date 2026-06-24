8643fb8ced7c43f75cd7a6a408e34a0442a136a5
Caleb Hu <calebhu552@gmail.com>
2026-06-23 20:25:51 -0700

    Add: hand-of-straights - submission-3

---
+++ Data Structures & Algorithms/hand-of-straights/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0) return false;
+        Map<Integer, Integer> cardFreq = new HashMap<>();
+        for (int card : hand) {
+            cardFreq.put(card, cardFreq.getOrDefault(card, 0) + 1);
+        }
+
+        for (int num : hand) {
+            int start = num;
+            while (cardFreq.getOrDefault(start - 1, 0) > 0) start--;
+            while (start <= num) {
+                while (cardFreq.getOrDefault(start, 0) > 0) {
+                    for (int i = start; i < start + groupSize; i++) {
+                        if (cardFreq.getOrDefault(i, 0) == 0) return false;
+                        cardFreq.put(i, cardFreq.get(i) - 1);
+                    }
+                }
+                start++;
+            }
+        }
+
+        return true;
+    }
+}
