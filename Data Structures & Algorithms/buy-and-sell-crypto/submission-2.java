1924cc482dfcb61f85669071ca82a8b40745bf40
Caleb Hu <calebhu552@gmail.com>
2026-07-23 16:58:35 -0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int l = 0;
+        int r = l + 1;
+        while (r < prices.length) {
+            int currProfit = prices[r] - prices[l];
+            if (currProfit < 0) {
+                l = r;
+            }
+            r++;
+            profit = Math.max(currProfit, profit);
+        }
+        return profit;
+    }
+}
