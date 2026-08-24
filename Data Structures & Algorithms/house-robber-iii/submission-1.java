3c04e28b7de77db4cd59e251faa4db8f944fb5f1
Caleb Hu <calebhu552@gmail.com>
2026-08-24 13:01:07 -0700

    Add: house-robber-iii - submission-1

---
+++ Data Structures & Algorithms/house-robber-iii/submission-1.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] res = postOrder(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    private int[] postOrder(TreeNode cur) {
+        if (cur == null) return new int[]{0, 0};
+
+        int[] left = postOrder(cur.left);
+        int[] right = postOrder(cur.right);
+
+        int rob = cur.val + left[1] + right[1];
+        int skip = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return new int[]{rob, skip};
+    }
+}
