0de0b1b0141d55f6164bd37cf3b2dd4ee348c627
Caleb Hu <calebhu552@gmail.com>
2026-09-10 13:31:16 -0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.java
@@ -0,0 +1,22 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        Collections.sort(intervals, (a, b) -> a.start - b.start);
+        int prevEnd = -1;
+        for (Interval i : intervals) {
+            if (prevEnd > i.start) return false;
+            prevEnd = i.end;
+        }
+        return true;
+    }
+}
