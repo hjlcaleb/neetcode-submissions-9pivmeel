b35466efa692c27609285baf21dda145a444f8f5
Caleb Hu <calebhu552@gmail.com>
2026-09-14 16:36:43 -0700

    Add: special-array-i - submission-0

---
+++ Data Structures & Algorithms/special-array-i/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (((nums[i] & 1) == 0 && (nums[i + 1] & 1) == 0) || 
+                ((nums[i] & 1) != 0 && (nums[i + 1] & 1) != 0)) {
+
+                return false;
+            }
+        }
+        return true;
+    }
+}
