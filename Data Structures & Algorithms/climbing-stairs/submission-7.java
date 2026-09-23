01e21782291f0066d169b57e535f81a14ff0aa3a
Caleb Hu <calebhu552@gmail.com>
2026-09-22 22:00:25 -0700

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i != n - 1) {
+                dp[i] = dp[i + 1] + dp[i + 2];
+            } else {
+                dp[i] = dp[i + 1];
+            }
+        }
+        return dp[0];
+    }
+}
